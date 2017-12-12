6cd9750fb886cfb4a449898cdfc79153b39dab0b
zhengpengfei_i <zhengpengfei_i@didichuxing.com>
2017-12-12 13:58:56 +0800

    cha03_decision tree

--- chp03/py_codes/tree.py
+++ chp03/py_codes/tree.py
@@ -1,5 +1,8 @@
 from math import log
 import operator
+import matplotlib.pyplot as plt
+import pickle
+
 def calShannonEntropy(dataSet):
     numberOfData = len(dataSet)
     labelDic = {}
@@ -42,7 +45,6 @@ def chooseBestFeature(dataSet):
     numberFeatures = len(dataSet[0])-1
     # calculation the ShannonEntropy as the base line
     baseEntorpy = calShannonEntropy(dataSet)
-
     #define the best result
     bestInfoGain = 0.0
     bestFeature = -1
@@ -98,11 +100,137 @@ def createTree(dataSet, labels):
     #
     for values in uniqueFeatureVals:
         # Python: parameters are passed by reference, so get a new param
-        subLabels = labels
+        subLabels = labels[:]
         mytree[bestFeatureLabel][values] = createTree(splitDataSet(dataSet,bestFeatureAxis,values),subLabels)
     return mytree
 
 myMat3, labels3 = createDataSet()
 print 'createtree:',createTree(myMat3,labels3)
 
+# get numbers of tree leaf
+def getTreeLeafs(myTree):
+    numberOfLeafs = 0
+    firstStr = myTree.keys()[0]
+    secondDic = myTree[firstStr]
+    for key in secondDic.keys():
+        if type(secondDic[key]).__name__=='dict':
+            numberOfLeafs += getTreeLeafs(secondDic[key])
+        else:
+            numberOfLeafs+=1
+    return numberOfLeafs
 
+# get tree depth
+def getTreeDepth(myTree):
+    maxDepth = 0
+    firstStr = myTree.keys()[0]
+    secondDic = myTree[firstStr]
+    for key in secondDic.keys():
+        if type(secondDic[key]).__name__=='dict':
+            thisDepth = 1 + getTreeDepth(secondDic[key])
+        else:
+            thisDepth = 1
+        if thisDepth > maxDepth : maxDepth = thisDepth
+    return maxDepth
+
+def retriveTree(i):
+    listOfTrees = [ {'no surfacing':{0:'no',1:{'flippers':{0:'no',1:'yes'}}}},
+                    {'no surfacing':{0:'no',1:{'flippers':{0:{'head':{0:'no',1:'yes'}},1:'no'}}}}]
+    return listOfTrees[i]
+
+mytree = retriveTree(0)
+print 'leafs:',getTreeLeafs(mytree)
+print 'depth:',getTreeDepth(mytree)
+
+
+
+
+decisionNode = dict(boxstyle = "sawtooth",fc='0.8')
+leafNode = dict(boxstyle='round4',fc='0.8')
+arrow_args = dict(arrowstyle='<-')
+def plotNode(nodeTxt, centerPt, parentPt, nodeType):
+
+    createPlot.ax1.annotate(nodeTxt,xy=parentPt,xycoords='axes fraction',xytext=centerPt,textcoords='axes fraction',
+                        va='center',ha='center',bbox=nodeType,arrowprops=arrow_args)
+
+def plotMidText(cntrPt, parentPt, txtString):
+    xMid = (parentPt[0] - cntrPt[0])/2.0 + cntrPt[0]
+    yMid = (parentPt[1] - cntrPt[1])/2.0 + cntrPt[1]
+    createPlot.ax1.text(xMid,yMid,txtString)
+
+def plotTree(mytree, parentPt, nodeTxt):
+    leafs = getTreeLeafs(mytree)
+    depth = getTreeDepth(mytree)
+    firstStr = mytree.keys()[0]
+    cntrPt = (plotTree.xOff + (1.0 + float(leafs))/2.0/plotTree.totalW, plotTree.yOff)
+    plotMidText(cntrPt,parentPt,nodeTxt)
+    plotNode(firstStr, cntrPt, parentPt, decisionNode)
+    secondDic = mytree[firstStr]
+    plotTree.yOff = plotTree.yOff - 1.0/plotTree.totalD
+    for key in secondDic.keys():
+        if type(secondDic[key]).__name__=='dict':
+            plotTree(secondDic[key],cntrPt,str(key))
+        else:
+            plotTree.xOff = plotTree.xOff + 1.0/plotTree.totalW
+            plotNode(secondDic[key],(plotTree.xOff,plotTree.yOff),cntrPt,leafNode)
+            plotMidText((plotTree.xOff,plotTree.yOff),cntrPt,str(key))
+    plotTree.yOff = plotTree.yOff + 1.0/plotTree.totalD
+
+def createPlot(inTree,save_name):
+    fig = plt.figure(1, facecolor='white')
+    fig.clf()
+    axprops = dict(xticks=[], yticks=[])
+    createPlot.ax1 = plt.subplot(111,frameon=False,**axprops)
+    plotTree.totalW = float(getTreeLeafs(inTree))
+    plotTree.totalD = float(getTreeDepth(inTree))
+    plotTree.xOff = -0.5/plotTree.totalW
+    plotTree.yOff = 1.0
+    plotTree(inTree,(0.5,1.0),'')
+    plt.show()
+    fig.savefig(save_name)
+
+mytree = retriveTree(0)
+#mytree['no surfacing'][3]='maybe'
+print mytree
+createPlot(mytree,'tree_result.jpg')
+#createPlot(mytree)
+
+def classify(inputTree, featLabels, testVec):
+    firstStr = inputTree.keys()[0]
+    secondDict = inputTree[firstStr]
+    featIndex = featLabels.index(firstStr)
+    for key in secondDict.keys():
+        if testVec[featIndex] == key:
+            if type(secondDict[key]).__name__=='dict':
+                classLabel = classify(secondDict[key],featLabels,testVec)
+            else:
+                classLabel = secondDict[key]
+    return classLabel
+
+mytree = retriveTree(0)
+myDat, labels = createDataSet()
+print classify(mytree,labels,[0,0])
+print classify(mytree,labels,[1,0])
+print classify(mytree,labels,[1,1])
+
+def storeTree(inputTree,filename):
+    fw = open(filename,'w')
+    pickle.dump(inputTree,fw)
+    fw.close()
+def grabTree(filename):
+    fr = open(filename)
+    return pickle.load(fr)
+
+storeTree(mytree,'classifierStorage.txt')
+print grabTree('classifierStorage.txt')
+
+
+fr = open('../data/lenses.txt')
+lenses = [inst.strip().split('\t') for inst in fr.readlines()]
+print lenses
+lensesLabels = ['age', 'prescript', 'astigmatic', 'tearRate']
+print lensesLabels
+lensesTree = createTree(lenses, lensesLabels)
+print lensesTree
+createPlot(lensesTree,'lenses_result.jpg')
+storeTree(lensesTree,'lensesTree.txt')
+print grabTree('lensesTree.txt')
