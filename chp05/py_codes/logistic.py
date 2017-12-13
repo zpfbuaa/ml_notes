1d833e4044b9dc54553ae7b612ec883852ed16ac
zhengpengfei_i <zhengpengfei_i@didichuxing.com>
2017-12-13 13:52:22 +0800

    add traing_data and horse_testing

--- chp05/py_codes/logistic.py
+++ chp05/py_codes/logistic.py
@@ -63,7 +63,11 @@ def plotBestFit(weights,name):
     ax.plot(x,y)
     plt.xlabel('X1')
     plt.ylabel('X2')
+    plt.title('Training Algorithm '+name)
+    plt.savefig(name + '.jpg')
     plt.show()
+    plt.close()
+
 
 dataMat, labelMat = loadDataSet()
 weights1 = gradAscent(dataMat,labelMat)
@@ -86,7 +90,7 @@ weights2 = stocGradAscent0(np.array(dataMat), labelMat)
 print ('weights2',weights2)
 plotBestFit(weights2,'stocGradAscent0')
 
-def stocGradAscent1(dataMatrix, classLabels, numIter = 150):
+def stocGradAscent1(dataMatrix, classLabels, numIter):
     column, row = np.shape(dataMatrix)
     weights = np.ones(row)
     for j in range(numIter):
@@ -100,7 +104,46 @@ def stocGradAscent1(dataMatrix, classLabels, numIter = 150):
             del(dataIndex[randIndex])
     return weights
 
-weights3 = stocGradAscent1(np.array(dataMat), labelMat)
+weights3 = stocGradAscent1(np.array(dataMat), labelMat,150)
 print ('weights3',weights3)
 plotBestFit(weights3,'stocGradAscent1')
 
+def classifyVector(inX, weights):
+    prob = sigmoid(sum( inX * weights))
+    if prob > 0.5 : return 1.0
+    else : return 0.0
+
+def colicTest():
+    frTrain = open('../data/horseColicTraining.txt')
+    frTest = open('../data/horseColicTest.txt')
+    trainingtSet = []; trainingLabels = []
+    for line in frTrain.readlines():
+        currLine = line.strip().split('\t')
+        lineArr = []
+        for i in range(21):
+            lineArr.append(float(currLine[i]))
+
+        trainingtSet.append(lineArr)
+        trainingLabels.append(float(currLine[21]))
+    trainWeights = stocGradAscent1(np.array(trainingtSet), trainingLabels, 500) # get trainWeights and set training rounds = 500
+    errorCount = 0; numTestVec = 0.0
+    for line in frTest.readlines():
+        numTestVec+=1.0
+        currLine = line.strip().split('\t')
+        lineArr = []
+        for i in range(21):
+            lineArr.append(float(currLine[i]))
+        if int(classifyVector(np.array(lineArr),trainWeights))!= int(currLine[21]):
+            errorCount+=1
+    errorRate = (float(errorCount)/numTestVec)
+    print ('the error rate of this test is: %f'%errorRate)
+    return errorRate
+
+def multiTest():
+    numTests = 10
+    errorSum = 0.0
+    for k in range(numTests):
+        errorSum += colicTest()
+    print 'after %d iteration the average error rate is: %f' %(numTests,errorSum/float(numTests))
+
+multiTest()
